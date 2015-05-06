f63ec3653d2ea0e856da756a4606e6e05e20da43
Justin Cunningham <justin@bulletprooftiger.com>
2015-05-05 21:13:13 -0700

    Added tarsnap name

--- metadata.rb
+++ metadata.rb
@@ -1,3 +1,4 @@
+name             "tarsnap"
 maintainer       "ZephirWorks"
 maintainer_email "andrea.campi@zephirworks.com"
 license          "Apache 2.0"
@@ -34,7 +35,7 @@ attribute "tarsnap/private_key",
   :display_name => "tarsnap private key",
   :description => "Path to the tarsnap private key file",
   :default => "/etc/tarsnap.key"
-  
+
 attribute "tarsnap/maxbw_rate",
   :display_name => "tarsnap max bandwidth rate",
   :description => "Limit download and upload bandwidth to bytespersecond",
@@ -44,17 +45,17 @@ attribute "tarsnapper/retention",
   :display_name => "tarsnapper retention policy",
   :description => "Retention policy of tarsnap backups",
   :default => "1d 7d 30d 3650d"
-  
+
 attribute "tarsnapper/jobs",
   :display_name => "tarsnapper backup jobs",
   :description => "Backup jobs tarsnapper runs",
   :default => '{"base" => {"sources" => ["/etc"]}}'
-  
+
 attribute "tarsnapper/cron/setup",
   :display_name => "tarsnapper cron enabled switch",
   :description => "tarsnapper cron creation policy",
   :default => "true"
-  
+
 {"minute" => "30", "hour" => "3", "day" => "Unset (Defaults to *)", "month" => "Unset (Defaults to *)", "weekday" => "Unset (Defaults to *)"}.each {|time, default|
   attribute "tarsnapper/cron/#{time}",
     :display_name => "tarsnapper cron #{time} schedule",
